849b3c9a407036d8920ecb0d533bd882c5cc1f45
toanlt <toankim12354@gmail.com>
2023-04-12 14:11:11 +0700

    Committer: mini crawler utility library

---
+++ crawler_utility/VnexpressParser.php
@@ -0,0 +1,22 @@
+<?php
+require 'Parser.php';
+class VnexpressParser extends Parser {
+    public function parse() {
+        $html = $this->get_html();
+        if (!empty($html)) {
+            $dom = new DOMDocument();
+            libxml_use_internal_errors(true);
+            $dom->loadHTML($html);
+            libxml_clear_errors();
+            $title = $dom->getElementsByTagName('h1')[0]->textContent;
+
+            $class = $this->getElementsByClass('fck_detail');
+            $content = $class;
+            $tg = $this->getElementsByClass('date');
+            $date = $tg;
+
+            return ['title' => $title, 'content' => $content, 'date' => $date];
+        }
+        return null;
+    }
+}
