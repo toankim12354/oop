fc8519fae8e58ecaa1ffd1caeae5a59d71724d21
toanlt <toankim12354@gmail.com>
2023-04-17 11:38:12 +0700

    update design pattern

---
+++ MVC/view/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Link</h2>
+<form method="get" action="">
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname" value=""><br>
+    <button type="submit">check</button>
+</form>
+</body>
+</html>
